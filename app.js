d498468f3060cb39fbe8fbc13b04b6acaeef8f10
Adi <nugrahaddict@gmail.com>
2026-03-05 11:46:36 +0700

    update v4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, CICD World v3 wow !');
+    res.end('Hello, CICD World v3 wow skibidi !');
 });
 
 server.listen(3000);
